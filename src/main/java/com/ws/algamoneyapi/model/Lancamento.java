dbe8fc7f0f127063852d1e3ca52b93c3800b09bd
william.silva <william@viasoft.com.br>
2018-07-17 15:49:40 -0300

    Criando a migração e entidade de lançamento

---
+++ src/main/java/com/ws/algamoneyapi/model/Lancamento.java
@@ -0,0 +1,44 @@
+package com.ws.algamoneyapi.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "lancamento")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "codigo")
+public class Lancamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private String descricao;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
+    @Column(name = "data_pagamento")
+    private LocalDate dataPagamento;
+
+    private BigDecimal valor;
+
+    private String observacao;
+
+    @Enumerated(EnumType.STRING)
+    private TipoLancamento tipo;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_categoria")
+    private Categoria categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_pessoa")
+    private Pessoa pessoa;
+}
---
+++ src/main/java/com/ws/algamoneyapi/model/TipoLancamento.java
@@ -0,0 +1,6 @@
+package com.ws.algamoneyapi.model;
+
+public enum TipoLancamento {
+    DESPESA,
+    RECEITA
+}
