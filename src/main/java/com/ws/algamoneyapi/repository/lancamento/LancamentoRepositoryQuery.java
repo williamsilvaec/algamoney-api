2c5fda9a4f05f0fe0b356c9578ae8d5b2809ea1f
william.silva <william@viasoft.com.br>
2018-07-18 13:17:13 -0300

    Implementando a paginação de lançamentos

--- src/main/java/com/ws/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/ws/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -2,10 +2,10 @@ package com.ws.algamoneyapi.repository.lancamento;
 
 import com.ws.algamoneyapi.model.Lancamento;
 import com.ws.algamoneyapi.repository.filter.LancamentoFilter;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface LancamentoRepositoryQuery {
 
-    List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+    Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
