0e4ba7638a7488c26386ee44c3f801a8ec731a19
william.silva <william@viasoft.com.br>
2018-07-19 10:03:47 -0300

    Movendo o refresh token do cookie para a requisição

---
+++ src/main/java/com/ws/algamoneyapi/token/RefreshTokenCookiePreProcessorFilter.java
@@ -0,0 +1,68 @@
+package com.ws.algamoneyapi.token;
+
+import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        if ("/oauth/token".equals(req.getRequestURI())
+                && "refresh_token".equals(req.getParameter("grant_type"))
+                && req.getCookies() != null) {
+
+            //pegar o cookie e colocar na requisição
+            for (Cookie cookie: req.getCookies()) {
+                if ("refreshToken".equals(cookie.getName())) {
+                    String refreshToken = cookie.getValue();
+                    req = new MyServletRequestWrapper(req, refreshToken);
+                }
+            }
+        }
+
+        chain.doFilter(req, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    static class MyServletRequestWrapper extends HttpServletRequestWrapper {
+
+        String refreshToken;
+
+        public MyServletRequestWrapper(HttpServletRequest request, String refreshToken) {
+            super(request);
+            this.refreshToken = refreshToken;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap() {
+            ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+            map.put("refresh_token", new String[]{this.refreshToken});
+            map.setLocked(true);
+            return map;
+        }
+    }
+}
